e79e21c9b877e8794ea904d4677ad045441c4f0a
zanussbaum <zanussbaum@gmail.com>
2022-09-10 11:25:27 -0400

    feat: process data smiles to selfies

---
+++ bio_lm/process_data/README.md
@@ -0,0 +1,6 @@
+# Data Processing Scripts
+
+This hosts the scripts used to process the data for the language model.
+Currently, it uses DeepChem to get the ZINC dataset and convert SMILES to SELFIES.
+
+On the 200K samples, it takes ~1min to process. 
---
+++ bio_lm/process_data/process_data.py
@@ -0,0 +1,65 @@
+import deepchem as dc
+from deepchem.molnet import load_zinc15
+import selfies as sf
+import argparse
+from tqdm import tqdm
+from enum import Enum
+from pathlib import Path
+
+
+tqdm.pandas()
+
+class DatasetName(Enum):
+    train = 0 
+    valid = 1
+    test = 2
+
+
+def load_dataset(size="250K"):
+    # just load raw data with smiles
+    featurizer = dc.feat.RawFeaturizer(smiles=True)
+    # datasets is a tuple of train, valid, test
+    tasks, datasets, transformer = load_zinc15(featurizer=featurizer)
+    train, valid, test = datasets
+
+    return tasks, (train, valid, test), transformer
+
+    
+def smiles_to_selfies(smile):
+    return sf.encoder(smile) 
+
+def create_dict(smiles):
+    selfies = smiles_to_selfies(smiles)
+    d = {"smiles": smiles, "selfies": selfies}
+    return d
+
+
+def process_data(dataset, path):
+    dataset = dataset.to_dataframe()
+    dataset["smile"] = dataset["X"]
+
+    dataset["selfie"] = dataset["smile"].progress_apply(smiles_to_selfies)
+
+    dataset.to_json(path, orient="records", lines=True)
+
+    
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--size", type=str, default="250K")
+    parser.add_argument("--dir", type=str, default="data/", help="directory to save data")
+    args = parser.parse_args()
+    return args
+
+    
+if __name__ == '__main__':
+    args = parse_args()
+    tasks, datasets, transformer = load_dataset(size=args.size)
+
+    folder = Path(args.dir)
+    if not folder.exists():
+        folder.mkdir(parents=True)
+
+    for i, d in enumerate(datasets):
+        name = DatasetName(i).name
+        path = f"{args.dir}_{name}_jsonl"
+        process_data(d, path)
