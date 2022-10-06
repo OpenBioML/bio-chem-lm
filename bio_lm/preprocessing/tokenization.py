d6d14dbe6a479228c49387b6ac77f9a3e29520eb
zanussbaum <zanussbaum@gmail.com>
2022-10-06 11:03:50 -0700

    refactor: formatting

--- bio_lm/preprocessing/tokenization.py
+++ bio_lm/preprocessing/tokenization.py
@@ -3,12 +3,12 @@ def tokenize_selfies(examples):
 
     selfies = examples["CAN_SELFIES"]
     for example in selfies:
-        location = example.find(']')
+        location = example.find("]")
         all_tokens = []
-        while location>=0:
-            all_tokens.append(example[0:location+1])
-            example = example[location+1:]
-            location = example.find(']')
+        while location >= 0:
+            all_tokens.append(example[0 : location + 1])
+            example = example[location + 1 :]
+            location = example.find("]")
 
         # to encode with tokenizers, input needs to be a string
         # easier to extract tokens and add whitespace then split in the tokenizer
@@ -17,12 +17,13 @@ def tokenize_selfies(examples):
 
     return {"tokenized": tokenized_examples}
 
-            
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     from datasets import load_dataset
+
     dataset = load_dataset("zpn/pubchem_selfies")
 
     train_ds = dataset["train"]
 
     item = train_ds[0]
-    print(tokenize_selfies(item['CAN_SELFIES']))
+    print(tokenize_selfies(item["CAN_SELFIES"]))
