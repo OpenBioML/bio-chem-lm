469dba36946f80ec5f47824829e190d4b9c962ca
Zach Nussbaum <zanussbaum@gmail.com>
2022-12-14 21:17:13 +0000

    refactor: clean up eval

--- bio_lm/evaluate.py
+++ bio_lm/evaluate.py
@@ -1,5 +1,4 @@
 import getpass
-import os
 
 import torch
 from accelerate import Accelerator
@@ -11,7 +10,7 @@ from bio_lm.dataset import load_finetune_dataset
 from bio_lm.metrics import (PROBLEM2METRICS, MetricDict, format_metrics,
                             name2metric)
 from bio_lm.model.classifier import ElectraForSequenceClassification
-from bio_lm.options import parse_args_finetune
+from bio_lm.options import parse_args_evaluate
 
 
 def evaluate(accelerator, config):
@@ -20,13 +19,12 @@ def evaluate(accelerator, config):
     tokenizer = AutoTokenizer.from_pretrained(config["tokenizer_name"])
 
     with accelerator.main_process_first():
-        # need to load train data to get mean and std
-        _, problem_type, num_labels, class_weights, mean, std = load_finetune_dataset(
-            config, tokenizer
-        )
-        test_dataloader, _, _, _, _, _ = load_finetune_dataset(
-            config, tokenizer, split="test", mean=mean, std=std
-        )
+        (
+            test_dataloader,
+            problem_type,
+            num_labels,
+            class_weights,
+        ) = load_finetune_dataset(config, tokenizer, split="test")
 
     model_config = AutoConfig.from_pretrained(config["model_name"])
     model_config.num_labels = num_labels
@@ -92,9 +90,9 @@ def evaluate(accelerator, config):
 
 
 if __name__ == "__main__":
-    accelerator = Accelerator()  # log_with="wandb")
+    accelerator = Accelerator(log_with="wandb")
 
-    args = parse_args_finetune()
+    args = parse_args_evaluate()
 
     config = {}
     config.update({k: v for k, v in args.__dict__.items()})
@@ -105,20 +103,21 @@ if __name__ == "__main__":
         config["wandb_entity"] = (
             config["wandb_entity"] if config["wandb_entity"] else getpass.getuser()
         )
+        if config["wandb_exp_name"] is None:
+            config[
+                "wandb_exp_name"
+            ] = f"{config['model_name']}_{config['dataset_name']}"
 
-    name = config["wandb_exp_name"] if "wandb_exp_name" in config else None
     accelerator.init_trackers(
         project_name=config["wandb_project"],
         config=config,
-        init_kwargs={"wandb": {"entity": config["wandb_entity"], "name": name}},
+        init_kwargs={
+            "wandb": {
+                "entity": config["wandb_entity"],
+                "name": config["wandb_exp_name"],
+            }
+        },
     )
-
-    if config["save_model"]:
-        # create save dir with random name
-        if not os.path.exists(config["save_dir"]):
-            # only save once per server (not sure if needed?)
-            if accelerator.is_local_main_process:
-                os.makedirs(config["save_dir"], exist_ok=True)
 
     accelerator.print("| configs: ")
     for k, v in config.items():
