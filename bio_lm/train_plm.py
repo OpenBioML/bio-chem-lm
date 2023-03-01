f9587a431c83ea32cc8712c8cccf054b2f6aa43a
Zach Nussbaum <zanussbaum@gmail.com>
2023-03-01 22:18:40 +0000

    feat: plm wip

---
+++ bio_lm/train_plm.py
@@ -0,0 +1,423 @@
+import getpass
+import os
+from functools import partial
+import numpy as np
+
+import torch
+import wandb
+from accelerate import Accelerator, DistributedType
+from accelerate.utils import set_seed
+from datasets import load_dataset
+from mup import MuAdam, set_base_shapes
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from transformers import (
+    AutoTokenizer,
+    DataCollatorForLanguageModeling,
+    get_linear_schedule_with_warmup,
+)
+
+from bio_lm.metrics import MetricDict, format_metrics
+from bio_lm.model.electra.modeling_electra import Electra, ElectraForPreTraining, ElectraForMaskedLM
+from bio_lm.model.electra.configuring_electra import ElectraConfig
+from bio_lm.options import parse_args
+from bio_lm.preprocessing.tokenization import preprocess_fn 
+from bio_lm.dataset import impute_aa
+from bio_lm.train_utils import (
+    print_token_diff,
+    load_config,
+    make_shapes,
+    tie_weights,
+    print_pred_replaced,
+)
+
+
+METRIC_NAMES = [
+    "loss",
+    "mlm_loss",
+    "disc_loss",
+    "gen_acc",
+    "disc_acc",
+    "disc_auprc",
+]
+
+
+def load_data(config, tokenizer, split="train"):
+    dataset = load_dataset(config["dataset_name"], split=split, streaming=True)
+    train_dset = dataset.filter(lambda _, idx: idx % 10 != 0, with_indices=True)
+    val_dset = dataset.filter(lambda _, idx: idx % 10 == 0, with_indices=True)
+
+    train_dset = train_dset.shuffle(seed=config["seed"], buffer_size=10_000)
+    train_dset = train_dset.map(
+        impute_aa, batched=True
+    )
+    train_dset = train_dset.map(
+        lambda x: preprocess_fn(x, tokenizer, column="imputed"),
+        batched=True,
+        remove_columns=[
+            "imputed",
+            "n",
+            "Tax",
+            "TaxID",
+            "RepID",
+            "sequence",
+            "__index_level_0__"
+        ],
+    )
+    val_dset = val_dset.shuffle(seed=config["seed"], buffer_size=10_000)
+    val_dset = val_dset.map(
+        impute_aa, batched=True
+    )
+    val_dset = val_dset.map(
+        lambda x: preprocess_fn(x, tokenizer, column="imputed"),
+        batched=True,
+        remove_columns=[
+            "imputed",
+            "n",
+            "Tax",
+            "TaxID",
+            "RepID",
+            "sequence",
+            "__index_level_0__"
+        ],
+    )
+
+    train_dset = train_dset.with_format("torch")
+    val_dset = val_dset.with_format("torch")
+
+    train_dataloader = DataLoader(
+        val_dset,
+        # we set mlm to false since we do MLM in the `Electra` model
+        collate_fn=DataCollatorForLanguageModeling(tokenizer, mlm=False, mlm_probability=0),
+        batch_size=config[f"batch_size"],
+    )
+
+    val_dataloader = DataLoader(
+        val_dset,
+        # we set mlm to false since we do MLM in the `Electra` model
+        collate_fn=DataCollatorForLanguageModeling(tokenizer, mlm=False, mlm_probability=0),
+        batch_size=config[f"batch_size"],
+    )
+
+    return train_dataloader, val_dataloader
+
+
+def train(accelerator, config):
+    set_seed(config["seed"])
+    accelerator.print(f"NUM GPUS: {accelerator.num_processes}")
+    tokenizer = AutoTokenizer.from_pretrained(config["tokenizer_name"], model_max_length=config["max_seq_length"])
+
+    with accelerator.main_process_first():
+        train_dataloader, val_dataloader = load_data(config, tokenizer)
+
+
+    if config["mup"]:
+        # generate the base shapes file
+        electra_shapes_filename = make_shapes(
+            base_size=config["base_config_size"],
+            delta_size="small.yaml",
+            config=config,
+            vocab_size=tokenizer.vocab_size,
+            pad_id=tokenizer.pad_token_id,
+            mask_id=tokenizer.mask_token_id,
+            save_dir=config["base_shapes_dir"],
+            arch_name="electra",
+        )
+
+        accelerator.print(f"Saved base shapes to {electra_shapes_filename}")
+
+        disc_config = load_config(config["discriminator_config"])
+        # update keys in config with values from cli
+        for key in disc_config:
+            if key in config:
+                disc_config[key] = config[key]
+
+        disc_config["mup"] = True
+        disc_config["vocab_size"] = tokenizer.vocab_size
+        disc_config["max_position_embeddings"] = config["max_seq_length"]
+        discriminator_config = ElectraConfig(**disc_config)
+        discriminator = ElectraForPreTraining(discriminator_config)
+
+        gen_config = load_config(config["generator_config"])
+        # update keys in config with values from cli
+        for key in gen_config:
+            if key in config:
+                gen_config[key] = config[key]
+
+        gen_config["mup"] = True
+        gen_config["vocab_size"] = tokenizer.vocab_size
+        gen_config["max_position_embeddings"] = config["max_seq_length"]
+        generator_config = ElectraConfig(**gen_config)
+        generator = ElectraForMaskedLM(generator_config)
+
+        tie_weights(generator, discriminator)
+
+        model = Electra(
+            discriminator=discriminator,
+            generator=generator,
+            pad_token_id=tokenizer.pad_token_id,
+            mask_token_id=tokenizer.mask_token_id,
+            config=discriminator_config,
+            mask_prob=config["mask_prob"],
+            disc_weight=config["disc_weight"],
+        )
+
+        set_base_shapes(model, electra_shapes_filename)
+
+    else:
+        disc_config = load_config(config["discriminator_config"])
+        disc_config["vocab_size"] = tokenizer.vocab_size
+        disc_config["max_position_embeddings"] = config["max_seq_length"]
+        discriminator_config = ElectraConfig(**disc_config)
+        discriminator = ElectraForPreTraining(discriminator_config)
+
+        gen_config = load_config(config["generator_config"])
+        gen_config["vocab_size"] = tokenizer.vocab_size
+        gen_config["max_position_embeddings"] = config["max_seq_length"]
+        generator_config = ElectraConfig(**gen_config)
+        generator = ElectraForMaskedLM(generator_config)
+
+        tie_weights(generator, discriminator)
+
+        model = Electra(
+            discriminator=discriminator,
+            generator=generator,
+            pad_token_id=tokenizer.pad_token_id,
+            mask_token_id=tokenizer.mask_token_id,
+            config=discriminator_config,
+            disc_weight=config["disc_weight"],
+        )
+
+    model.apply(
+        partial(
+            model._init_weights,
+            readout_zero_init=config["readout_zero_init"],
+            query_zero_init=config["query_zero_init"],
+        )
+    )
+
+    model.apply(
+        partial(
+            model._init_weights,
+            readout_zero_init=config["readout_zero_init"],
+            query_zero_init=config["query_zero_init"],
+        )
+    )
+
+    device = accelerator.device
+    model.to(device)
+
+    if config["mup"]:
+        optimizer = MuAdam(model.parameters(), lr=config["lr"])
+    else:
+        optimizer = Adam(model.parameters(), lr=config["lr"])
+
+    if config["scheduler"]:
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=config["num_warmup_steps"],
+            num_training_steps=config["num_training_steps"],
+        )
+
+    train_metrics = MetricDict(
+        METRIC_NAMES,
+        device=device,
+    )
+    val_metrics = MetricDict(
+        METRIC_NAMES,
+        device=device,
+    )
+
+    if config["scheduler"]:
+        (
+            model,
+            optimizer,
+            train_dataloader,
+            val_dataloader,
+            scheduler,
+        ) = accelerator.prepare(
+            model, optimizer, train_dataloader, val_dataloader, scheduler
+        )
+    else:
+        model, optimizer, train_dataloader, val_dataloader = accelerator.prepare(
+            model, optimizer, train_dataloader, val_dataloader
+        )
+
+    accelerator.print(f"Training a {model.num_parameters(exclude_embeddings=True):,} parameter model")
+    for epoch in range(config["num_epochs"]):
+        for step, batch in enumerate(
+            tqdm(
+                train_dataloader,
+                total=config["num_steps_per_epoch"],
+                desc="Training",
+                disable=not accelerator.is_local_main_process,
+            )
+        ):
+            if step == 5 and config["debug"]:
+                break
+
+            if step == config["num_steps_per_epoch"]:
+                break
+
+            optimizer.zero_grad()
+            model.train()
+
+            loss = model(**batch)
+            accelerator.backward(loss["loss"])
+
+            if config["global_clip_norm"] and config["global_clip_norm"] > 0:
+                if accelerator.distributed_type == DistributedType.DEEPSPEED:
+                    if accelerator.sync_gradients:
+                        accelerator.clip_grad_norm_(
+                            model.parameters(), config["global_clip_norm"]
+                        )
+                else:
+                    torch.nn.utils.clip_grad_norm_(
+                        model.parameters(),
+                        config["global_clip_norm"],
+                        error_if_nonfinite=True,
+                    )
+
+            optimizer.step()
+
+            if config["scheduler"]:
+                scheduler.step()
+
+            # filter here
+            loss_values = {
+                key: {"value": value.detach()}
+                for key, value in loss.items()
+                if key in METRIC_NAMES
+            }
+            loss_values = accelerator.gather_for_metrics(loss_values)
+
+            train_metrics.update(loss_values)
+
+        with torch.no_grad():
+            # we only evalute with N steps since there are 10M data points!!
+            for i, batch in enumerate(
+                tqdm(
+                    val_dataloader,
+                    desc="Validation",
+                    total=config["num_steps_per_epoch"],
+                    disable=not accelerator.is_local_main_process,
+                )
+            ):
+                if i == 5 and config["debug"]:
+                    break
+
+                if i == config["num_steps_per_epoch"]:
+                    break
+
+                model.eval()
+                loss = model(**batch)
+
+                # filter here
+                loss_values = {
+                    key: {"value": value.detach()}
+                    for key, value in loss.items()
+                    if key in METRIC_NAMES
+                }
+                loss_values = accelerator.gather_for_metrics(loss_values)
+
+                # add logging to see what predictions are, are we prediction only things like C?
+
+                val_metrics.update(loss_values)
+
+                if i < 5 and config["log_predictions"]:
+                    inputs = batch["input_ids"]
+                    masked_input = loss["masked_input"]
+                    disc_input = loss["disc_input"]
+                    disc_preds = loss["disc_predictions"]
+                    random_idx = np.random.randint(inputs.shape[0])
+                    print_token_diff(
+                        inputs, tokenizer, inputs, random_idx, prepend="INPUTS"
+                    )
+                    print_token_diff(
+                        masked_input,
+                        tokenizer,
+                        inputs,
+                        random_idx,
+                        prepend="MASKED_INPUTS",
+                    )
+                    print_token_diff(
+                        disc_input,
+                        tokenizer,
+                        inputs,
+                        random_idx,
+                        "disc_input",
+                        prepend="GENERATOR",
+                    )
+                    print_pred_replaced(
+                        disc_input,
+                        tokenizer,
+                        disc_preds.type(torch.bool),
+                        (disc_input != inputs),
+                        random_idx,
+                    )
+
+        log_train = {
+            f"train_{key}": value for key, value in train_metrics.compute().items()
+        }
+        log_val = {f"val_{key}": value for key, value in val_metrics.compute().items()}
+
+        if config["wandb"]:
+            accelerator.log({**log_train, **log_val})
+
+        accelerator.print(format_metrics(log_train, "train", f" epoch {epoch} "))
+        accelerator.print(format_metrics(log_val, "val", f" epoch {epoch} "))
+
+        train_metrics.reset_metrics()
+        val_metrics.reset_metrics()
+
+        if config["save_model"]:
+            accelerator.wait_for_everyone()
+            unwrapped_model = accelerator.unwrap_model(model)
+            unwrapped_model.save_pretrained(
+                f"{config['save_dir']}_epoch_{epoch}",
+                is_main_process=accelerator.is_main_process,
+                save_function=accelerator.save,
+                state_dict=unwrapped_model.state_dict(),
+            )
+
+    if config["wandb"]:
+        accelerator.end_training()
+
+
+if __name__ == "__main__":
+
+    args = parse_args()
+
+    config = {}
+    config.update({k: v for k, v in args.__dict__.items()})
+
+    if config["wandb"] or config["wandb_entity"]:
+        accelerator = Accelerator(log_with="wandb")
+        # if we set wandb_entity, we set to True automatically
+        config["wandb"] = True
+        config["wandb_entity"] = (
+            config["wandb_entity"] if config["wandb_entity"] else getpass.getuser()
+        )
+
+        name = config["wandb_exp_name"] if "wandb_exp_name" in config else None
+        accelerator.init_trackers(
+            project_name=config["wandb_project"],
+            config=config,
+            init_kwargs={"wandb": {"entity": config["wandb_entity"], "name": name}},
+        )
+    else:
+        accelerator = Accelerator()
+
+    if config["save_model"]:
+        # create save dir with random name
+        if not os.path.exists(config["save_dir"]):
+            # only save once per server (not sure if needed?)
+            if accelerator.is_local_main_process:
+                os.makedirs(config["save_dir"], exist_ok=True)
+
+    accelerator.print("| configs: ")
+    for k, v in config.items():
+        accelerator.print("  |", k, " : ", v)
+
+    train(accelerator=accelerator, config=config)
